e553e08c336326e4bdbac49bb09246d40de193d4
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-04-26 20:31:46 +0400

    basic html form for User.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
   def new; end
 
-  def create; end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
