a61c2eae62e1aa9e40cd40178062fa366e0e44b6
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-04-27 16:51:15 +0400

    Railsy Forms with #form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@
 
 class UsersController < ApplicationController
   def create
-    @user = User.new(user_params)
-
+    # @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -19,3 +19,14 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 end
+
+
+# def create
+#   @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+#   if @user.save
+#     redirect_to new_user_path
+#   else
+#     render :new, status: :unprocessable_entity
+#   end
+# end
